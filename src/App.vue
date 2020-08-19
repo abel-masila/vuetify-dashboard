738c20db15fe5f50a0d37c115cf5d40fdd207977
Abel Masila <abelmasila@gmail.com>
2020-08-19 23:33:48 +0300

    added dashboard link and table

--- src/App.vue
+++ src/App.vue
@@ -3,51 +3,33 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        text
+        rounded
+        :to="link.url"
+        :key="`${link.label}-appbar`"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
-    <!-- Login Module -->
+
     <v-main>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">
-            Register
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">
-            Login
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-main>
 
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -64,7 +46,20 @@ export default {
   data: () => {
     return {
       showPassword: false,
-      links: ["Home", "Login"],
+      links: [
+        {
+          label: "Home",
+          url: "/",
+        },
+        {
+          label: "Login",
+          url: "/login",
+        },
+        {
+          label: "Dashboard",
+          url: "/dashboard",
+        },
+      ],
     };
   },
 };
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Dashboard.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
