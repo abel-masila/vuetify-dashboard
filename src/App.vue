5e7c838a3c8d4e5dd4381f25bf31447707b0aab5
Abel Masila <abelmasila@gmail.com>
2020-08-20 02:45:58 +0300

    theming

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
       >
         {{ link.label }}
       </v-btn>
+      <v-btn @click="toggleTheme" text rounded>
+        Toggle Theme
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -65,6 +68,12 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.themes.dark.anchor = "#41B883";
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
   },
 };
 </script>
