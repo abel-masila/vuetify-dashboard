1f28876b98c34dd72caf357b4f88ba410a586b10
Abel Masila <abelmasila@gmail.com>
2020-08-20 02:20:36 +0300

    form validation

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col>
         <h1>Register</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
+        <v-form ref="registerForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
           <v-autocomplete
             label="Which browser do you use?"
             :items="browsers"
@@ -16,8 +22,26 @@
             readonly=""
           ></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms and conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms and conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn
+            type="submit"
+            color="primary"
+            class="mr-4"
+            :disabled="!formValidity"
+            >Submit</v-btn
+          >
+          <v-btn color="success" @click="validateForm" class="mr-4">
+            Validate Form
+          </v-btn>
+          <v-btn color="warning" @click="resetValidation" class="mr-4"
+            >Reset Validation</v-btn
+          >
+          <v-btn color="error" @click="resetForm">Reset Form</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -30,7 +54,41 @@ export default {
     return {
       browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
       birthday: "",
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          "You must agree to the terms and conditions to sign up for an account.",
+      ],
+      email: "",
+      emailRules: [
+        (value) => !!value || "Email is required!",
+        (value) =>
+          (value && value.indexOf("@") !== 0) ||
+          "Email should have a username.",
+        (value) =>
+          (value && value.includes(".")) ||
+          "Email should include a period symbol.",
+        (value) =>
+          (value && value.indexOf(".") - value.indexOf("@") > 1) ||
+          "Email should contain a valid domain.",
+        (value) =>
+          (value && value.indexOf(".") <= value.length - 3) ||
+          "Email should contain a valid domain extension.",
+      ],
+      formValidity: false,
     };
+  },
+  methods: {
+    resetForm() {
+      this.$refs.registerForm.reset();
+    },
+    resetValidation() {
+      this.$refs.registerForm.resetValidation();
+    },
+    validateForm() {
+      this.$refs.registerForm.validate();
+    },
   },
 };
 </script>
