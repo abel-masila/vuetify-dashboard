970db34b6b20a933a1d193a6e647a99d665c1d29
Abel Masila <abelmasila@gmail.com>
2020-08-20 02:33:10 +0300

    Amazing directives

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -20,6 +20,33 @@
         <EventTimeline :timeline="timeline" />
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row id="more-content" v-if="loadNewContent">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
 
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
@@ -61,6 +88,7 @@ export default {
       snackbar: false,
       statistics: statisticsData,
       timeline: timelineData,
+      loadNewContent: false,
     };
   },
   methods: {
@@ -68,6 +96,9 @@ export default {
       this.snackbar = true;
       this.selectedEmployee.name = event.name;
       this.selectedEmployee.title = event.title;
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting;
     },
   },
 };
